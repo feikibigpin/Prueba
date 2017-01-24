207beeef138d0ad9453a79542098c53eb5418c72
feikibigpin <diegoalonsomontoya@gmail.com>
2017-01-24 11:01:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,20 +2,20 @@ var dots = [];
 
 var value = 0;  //starting value of earthquake
 
-var button1;
-var button2;
-var button3;
-var myChile;
-var myJapon;
-var myIndonesia;
-var myMexico;
-var myResults;
+//var button1;
+//var button2;
+//var button3;
+//var myChile;
+//var myJapon;
+//var myIndonesia;
+//var myMexico;
+//var myResults;
 
-function preload() {
-    myChile = loadImage("images/9.5.png");
-    myJapon = loadImage("images/9.0.png");  
-    myIndonesia = loadImage("images/8.6.png");
-    myMexico = loadImage("images/8.1.png");
+//function preload() {
+    //myChile = loadImage("images/9.5.png");
+    //myJapon = loadImage("images/9.0.png");  
+    //myIndonesia = loadImage("images/8.6.png");
+    //myMexico = loadImage("images/8.1.png");
 
 function setup(){
      createCanvas(windowWidth, windowHeight);
