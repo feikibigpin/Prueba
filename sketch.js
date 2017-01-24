0f294e8eb9b9edb57b4d85b0519811f77895e6f3
feikibigpin <diegoalonsomontoya@gmail.com>
2017-01-24 12:59:13 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -106,7 +106,7 @@ function deviceShaken(){
     button3.touchStarted(clearEverything);
     
     button1 = createButton("See results");
-    button1.position(width/2,height/3);
+    button1.position((width/8)*5, (height/15)*14);
     button1.touchStarted(results); //en vez de mousePressed para touch es touchStarted
     /*
     button3 = createButton("try again");
